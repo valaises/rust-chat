ecf49b8a63b00b6957e2f451c6146bcd3aa2774e
V4LER11 <work@valerii.cc>
2025-05-27 20:57:00 +0100

    fix readme, api key

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ project/
 Run the following command in the root of your project to start developing with the default platform:
 
 ```bash
-dx serve --platform web
+dx serve --platform web --package core
 ```
 
 To run for a different platform, use the `--platform platform` flag. E.g.
--- crates/core/src/globals.rs
+++ crates/core/src/globals.rs
@@ -1,6 +1,6 @@
 
-pub static BACKEND_URL: &str = "https://llmtools.valerii.cc/v1";
+pub static BACKEND_URL: &str = "https://llmproxy.xi.valerii.cc/v1";
 pub const API_KEY: &str = match option_env!("REMOTE_VALERIICC_API_KEY") {
-    Some(endpoint) => endpoint,
-    None => "http://default-endpoint.com",
+    Some(api_key) => api_key,
+    None => "admin1234",
 };
